0e61d78aeaba3b65b31e1f8d14e53dba6a52d039
caputdraconis <caputdraconis@kakao.com>
2024-05-28 18:14:14 +0900

    Update bedrock_api.py

--- labs/api/bedrock_api.py
+++ labs/api/bedrock_api.py
@@ -2,39 +2,39 @@ import os
 import json
 import boto3
 
+#
 session = boto3.Session(
     profile_name=os.environ.get("BWB_PROFILE_NAME")
-) #AWS 자격 증명에 사용할 프로필 이름 설정
+) #sets the profile name to use for AWS credentials
 
 bedrock = session.client(
-    service_name='bedrock-runtime', #Bedrock client 생성
+    service_name='bedrock-runtime', #creates a Bedrock client
     region_name=os.environ.get("BWB_REGION_NAME"),
     endpoint_url=os.environ.get("BWB_ENDPOINT_URL")
 ) 
 
-bedrock_model_id = "anthropic.claude-3-sonnet-20240229-v1:0" #파운데이션 모델 설정
+#
+bedrock_model_id = "ai21.j2-ultra-v1" #set the foundation model
 
-prompt = "Human:뉴햄프셔에서 가장 큰 도시가 어디인가요? Assistant:" #모델에 보낼 프롬프트 설정
+prompt = "What is the largest city in New Hampshire?" #the prompt to send to the model
 
 body = json.dumps({
-    "anthropic_version": "bedrock-2023-05-31",
-    "max_tokens": 1024, 
-    "temperature": 0,
-    "messages": {
-                "role": "user",
-                "content": [
-                    {
-                        "type": "text",
-                        "text": prompt
-                    }
-                ]
-            },
-}) #요청 payload 설정
+    "prompt": prompt, #AI21
+    "maxTokens": 1024, 
+    "temperature": 0, 
+    "topP": 0.5, 
+    "stopSequences": [], 
+    "countPenalty": {"scale": 0 }, 
+    "presencePenalty": {"scale": 0 }, 
+    "frequencyPenalty": {"scale": 0 }
+}) #build the request payload
 
+#
+response = bedrock.invoke_model(body=body, modelId=bedrock_model_id, accept='application/json', contentType='application/json') #send the payload to Bedrock
 
-response = bedrock.invoke_model(body=body, modelId=bedrock_model_id, accept='application/json', contentType='application/json') #payload를 Bedrock으로 전송
+#
+response_body = json.loads(response.get('body').read()) # read the response
 
-response_body = json.loads(response.get('body').read()) # response 읽기
-results = response_body.get("content")[0].get("text")
+response_text = response_body.get("completions")[0].get("data").get("text") #extract the text from the JSON response
 
-print(results)
+print(response_text)
